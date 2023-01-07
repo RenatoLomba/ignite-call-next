fc7f97d974d761235c1d1722ed019e27d124ba92
RenatoLomba <rntlomba@gmail.com>
2023-01-07 14:19:42 -0300

    react hook form

--- src/pages/home/components/claim-username-form.tsx
+++ src/pages/home/components/claim-username-form.tsx
@@ -1,6 +1,9 @@
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-import { Box, Button, styled, TextInput } from '@ignite-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Box, Button, styled, Text, TextInput } from '@ignite-ui/react'
 
 const FormContainer = styled(Box, {
   marginTop: '$4',
@@ -14,10 +17,52 @@ const FormContainer = styled(Box, {
   },
 })
 
+const ErrorText = styled(Text, {
+  display: 'block',
+  color: '#e53e3e',
+  marginTop: '$1',
+})
+
+const formSchema = z.object({
+  username: z
+    .string({
+      required_error: 'Campo obrigatório',
+    })
+    .min(1, { message: 'Mínimo de 1 caractere' }),
+})
+
+type FormFields = z.infer<typeof formSchema>
+
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const onFormSubmit = async (data: FormFields) => {
+    console.log({ data })
+  }
+
   return (
-    <FormContainer as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="Seu usuário" />
+    <FormContainer as="form" onSubmit={handleSubmit(onFormSubmit)}>
+      <div>
+        <TextInput
+          {...register('username')}
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="Seu usuário"
+        />
+
+        {errors.username ? (
+          <ErrorText as="small" size="sm">
+            {errors.username.message}
+          </ErrorText>
+        ) : null}
+      </div>
+
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight size={20} />
