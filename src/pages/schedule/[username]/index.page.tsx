1d8ee05f1638ea11a6596db4f1b7c84422c3728b
RenatoLomba <rntlomba@gmail.com>
2023-01-10 16:22:36 -0300

    get static pages

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,76 @@
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from 'next'
+
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+
+import { prisma } from '../../../lib/prisma'
+import { Container, Header } from './styles'
+
+// #region GET_STATIC_PROPS
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    select: {
+      name: true,
+      avatar_url: true,
+      bio: true,
+      id: true,
+    },
+    where: { username },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        avatarUrl: user.avatar_url,
+        bio: user.bio,
+        id: user.bio,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
+// #endregion
+
+// #region GET_STATIC_PATHS
+export const getStaticPaths: GetStaticPaths = async () => {
+  const users = await prisma.user.findMany({
+    take: 20,
+    orderBy: {
+      created_at: 'desc',
+    },
+  })
+
+  return {
+    paths: users.map((user) => ({
+      params: { username: user.username },
+    })),
+    fallback: 'blocking',
+  }
+}
+// #endregion
+
+export default function SchedulePage({
+  user,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Container>
+      <Header>
+        <Avatar src={user.avatarUrl!} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </Header>
+    </Container>
+  )
+}
