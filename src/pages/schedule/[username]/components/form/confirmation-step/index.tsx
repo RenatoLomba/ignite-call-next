081995e3be44a235baa252af58d857afff0864ac
RenatoLomba <rntlomba@gmail.com>
2023-01-10 19:00:56 -0300

    form validation confirmation step

--- src/pages/schedule/[username]/components/form/confirmation-step/index.tsx
+++ src/pages/schedule/[username]/components/form/confirmation-step/index.tsx
@@ -5,24 +5,30 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
 
-import { Actions, Container, Header } from './styles'
+import { Actions, Container, FormErrorText, Header } from './styles'
 
 const formSchema = z.object({
   name: z.string().min(3, { message: 'Mínimo de 3 caracteres' }),
-  email: z.string().email({ message: 'Insira um e-mail valido' }),
-  observations: z.string(),
+  email: z.string().email({ message: 'Insira um e-mail válido' }),
+  observations: z.string().optional(),
 })
 
 type FormFields = z.infer<typeof formSchema>
 
 export function ConfirmationStep() {
-  const { handleSubmit, register } = useForm<FormFields>({
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
     resolver: zodResolver(formSchema),
   })
 
   const onFormSubmit = handleSubmit(async (data) => {
     console.log(data)
   })
+
+  const isLoading = isSubmitting
 
   return (
     <Container as="form" onSubmit={onFormSubmit}>
@@ -39,11 +45,19 @@ export function ConfirmationStep() {
       <label>
         <Text size="sm">Nome completo</Text>
         <TextInput placeholder="Seu nome" {...register('name')} />
+
+        {errors.name && (
+          <FormErrorText size="sm">{errors.name.message}</FormErrorText>
+        )}
       </label>
 
       <label>
         <Text size="sm">Endereço de e-mail</Text>
         <TextInput placeholder="johndoe@example.com" {...register('email')} />
+
+        {errors.email && (
+          <FormErrorText size="sm">{errors.email.message}</FormErrorText>
+        )}
       </label>
 
       <label>
@@ -56,7 +70,9 @@ export function ConfirmationStep() {
           Cancelar
         </Button>
 
-        <Button type="submit">Confirmar</Button>
+        <Button disabled={isLoading} type="submit">
+          Confirmar
+        </Button>
       </Actions>
     </Container>
   )
